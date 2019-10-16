c836fff0e082de204134c154133ceffdecca0548
Eliakin Alves <eliakin.alves.dev@gmail.com>
2019-10-16 16:20:00 -0300

    nova versao

--- paginas dinamicas/dados.php
+++ paginas dinamicas/dados.php
@@ -1,8 +1,8 @@
 <?php
     // Entrada -> form.html, metodo -> get (via URL)
-    $n1 = $_GET['numero1'];
-    $n2 = $_GET['numero2'];
-    $n3 = $_GET['numero3'];
+    $n1 = @$_GET['numero1'];
+    $n2 = @$_GET['numero2'];
+    $n3 = @$_GET['numero3'];
     // processamento
     $resultado = ($n1 + $n2 + $n3) / 3;
     //Saída
